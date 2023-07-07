aecb4b62be18944455d35c066fa113db5d6afe28
yakhimm <20120307@student.hcmus.edu.vn>
2023-07-08 00:01:58 +0700

    update website

--- app.py
+++ app.py
@@ -8,6 +8,8 @@ import numpy as np
 
 import random # inbuilt module
 
+st.set_option('browser.gatherUsageStats', False)
+
 #=================================== Title ===============================
 st.title("""
 Cat 🐱 Or Dog 🐶
